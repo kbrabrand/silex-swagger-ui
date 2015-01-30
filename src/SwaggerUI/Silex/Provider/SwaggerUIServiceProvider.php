0f7c5e9452db0ff935b372c9db4e42f1893bb8da
ALeX <alessandro.lucaferro@gmail.com>
2015-01-30 09:29:54 +0100

    Update SwaggerUIServiceProvider.php
    Removed unnecessary newlines and spaces.

--- src/SwaggerUI/Silex/Provider/SwaggerUIServiceProvider.php
+++ src/SwaggerUI/Silex/Provider/SwaggerUIServiceProvider.php
@@ -1,12 +1,10 @@
 <?php
-
 namespace SwaggerUI\Silex\Provider;
-
 use Silex\Application;
 use Silex\ServiceProviderInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-
 /**
  * The SwaggerUIServiceProvider adds views for swagger UI to a silex app, making
  * it possible to view swagger docs.
@@ -20,51 +18,47 @@ class SwaggerUIServiceProvider implements ServiceProviderInterface
      */
     public function boot(Application $app)
     {
+        // Reference to $this, it's used for closure in anonymous function (PHP 5.3.x)
         $self = &$this;
-
-        $app->get($app['swaggerui.path'], function() use ($app) {
-
-
-            return str_replace(array('{{swaggerui-root}}', '{{swagger-docs}}'), array($app['request']->getBaseUrl().$app['swaggerui.path'],$app['request']->getBaseUrl(). $app['swaggerui.apiDocPath']), file_get_contents(__DIR__ . '/../../../../public/index.html'));
-        });
-
-        $app->get($app['swaggerui.path'] . '/{resource}', function($resource) use ($app,$self) {
-            return $self->getFile(
-                __DIR__ . '/../../../../public/' . $resource,
-                'text/css'
+        $app->get($app['swaggerui.path'], function(Request $request) use ($app) {
+            return str_replace(
+                array('{{swaggerui-root}}', '{{swagger-docs}}'),
+                array($request->getBasePath() . $app['swaggerui.path'], $request->getBasePath() . $app['swaggerui.apiDocPath']),
+                file_get_contents(__DIR__ . '/../../../../public/index.html')
             );
         });
-
-        $app->get($app['swaggerui.path'] . '/lib/{resource}', function($resource) use ($app,$self) {
+        $app->get($app['swaggerui.path'] . '/{resource}', function($resource) use ($app) {
+            $file = __DIR__ . '/../../../../public/' . $resource;
+            if (is_file($file)) {
+                return file_get_contents($file);
+            }
+            return '';
+        });
+        $app->get($app['swaggerui.path'] . '/lib/{resource}', function($resource) use ($app, $self) {
             return $self->getFile(
                 __DIR__ . '/../../../../public/lib/' . $resource,
                 'text/javascript'
             );
         });
-
-        $app->get($app['swaggerui.path'] . '/css/{resource}', function($resource) use ($app,$self) {
+        $app->get($app['swaggerui.path'] . '/css/{resource}', function($resource) use ($app, $self) {
             return $self->getFile(
                 __DIR__ . '/../../../../public/css/' . $resource,
                 'text/css'
             );
         });
-
-     
-        $app->get($app['swaggerui.path'] . '/images/{resource}', function($resource) use ($app,$self) {
+        $app->get($app['swaggerui.path'] . '/images/{resource}', function($resource) use ($app, $self) {
             return $self->getFile(
                 __DIR__ . '/../../../../public/images/' . $resource,
                 'image/png'
             );
         });
     }
-
     /**
      * Registers the swagger UI service
      *
      * @param Application $app
      */
     public function register(Application $app) {}
-
     /**
      * Get a public file
      *
@@ -79,7 +73,6 @@ class SwaggerUIServiceProvider implements ServiceProviderInterface
             $response->setCharset('UTF-8');
             return $response;
         }
-
         return new Response('', 404);
     }
 }
